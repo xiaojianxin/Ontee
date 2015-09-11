13d4301e15781e1ebdebedfc5b1b75c7809f6db0
xiaojianxin <335383263@qq.com>
2015-09-11 15:16:58 +0800

    xiugai

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -5,18 +5,18 @@ use yii\base\Model;
 use yii\web\UploadedFile;
 
 /**
-* UploadForm is the model behind the upload form.
-*/
+ * UploadForm is the model behind the upload form.
+ */
 class UploadForm extends Model
 {
     /**
-    * @var UploadedFile file attribute
-    */
+     * @var UploadedFile|Null file attribute
+     */
     public $file;
 
     /**
-    * @return array the validation rules.
-    */
+     * @return array the validation rules.
+     */
     public function rules()
     {
         return [
