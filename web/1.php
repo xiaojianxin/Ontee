700a37eecfd673abc3c6f2e75981b71ad158714d
xiaojianxin <335383263@qq.com>
2015-09-14 12:01:15 +0800

    11

--- web/1.php
+++ web/1.php
@@ -1,4 +1,4 @@
 <form action="/save/upload" method="POST" enctype="multipart/form-data">
-	<input type="file" name="UploadForm[file]"/>
+	<input type="file" name="file"/>
 	<input type="submit" value="submit"/>
 </form>
