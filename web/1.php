e4e8ab503fa0a64d89830ec4fd69996f4386029c
xiaojianxin <335383263@qq.com>
2015-09-14 11:58:49 +0800

    ugai

--- web/1.php
+++ web/1.php
@@ -1,4 +1,4 @@
-<form action="./site/upload" method="POST" enctype="multipart/form-data">
+<form action="/save/upload" method="POST" enctype="multipart/form-data">
 	<input type="file" name="UploadForm[file]"/>
 	<input type="submit" value="submit"/>
 </form>
