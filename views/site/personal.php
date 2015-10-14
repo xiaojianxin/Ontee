511350c02f49a899403f9eb24445ecefe2599630
xiaojianxin <335383263@qq.com>
2015-10-14 12:25:28 +0800

    1

--- views/site/personal.php
+++ views/site/personal.php
@@ -17,7 +17,7 @@ $this->title = 'Ontee';
     <div class="userTitle">
         <div class="userPic">
             <div class="img-thumbnail" id="showUserHead">
-                <?php if(empty($user->facepic)){?>
+                <?php if($user->facepic===""){?>
                     <img src="/<?=$user->facepic?>"/>
                 <?php }else{?>
                     <img src="/images/default.png"/>
