c09f3b205efe3e01ae3d89da26851140fcfd9fcb
xiaojianxin <335383263@qq.com>
2015-11-10 11:23:09 +0800

    previous

--- web/js/choose.js
+++ web/js/choose.js
@@ -391,6 +391,7 @@ editTee=function(){
                   if(data=="404")
                   {
                       alertify.error("请先登录");
+                      break;
                   }
                   else if(data=="0")
                   {
